ede9f7c3c63f9f48829aa852bbfd331431afa50d
Miftah Regyana <miftahregyana@gmail.com>
2020-08-25 10:58:02 +0700

    rename routes to /api/company

--- lib/config/routes.ts
+++ lib/config/routes.ts
@@ -7,12 +7,12 @@ export class Routes {
 
   public routes(app: Application): void {
     app
-      .route("/company")
+      .route("/api/company")
       .get(this.companyController.index)
       .post(this.companyController.create);
 
     app
-      .route("/company/:id")
+      .route("/api/company/:id")
       .get(this.companyController.show)
       .put(this.companyController.update)
       .delete(this.companyController.delete);
